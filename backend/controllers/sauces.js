2d8c37e56bbc918026c00943f82053caa1d4e8b3
stefzouille <contact.lokcy@gmail.com>
2022-05-23 14:00:24 +0200

    ad

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -13,11 +13,11 @@ exports.creatSauce = (req, res, next) => {
   const sauce = new Sauce({
     ...sauceObject,
     // generer l url de l image
-    // imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-    likes: 0,
-    dislikes: 0,
-    usersLiked: [],
-    usersDisliked: []
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    // likes: 0,
+    // dislikes: 0,
+    // usersLiked: [],
+    // usersDisliked: []
   });
   sauce.save()
     .then(() => res.status(201).json({
